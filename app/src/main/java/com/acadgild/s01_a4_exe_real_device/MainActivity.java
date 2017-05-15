2bc53645ecb47e75c9044f4f9057984bdfe41c24
admiringsunny <admiringsunny@gmail.com>
2017-05-15 20:37:13 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/s01_a4_exe_real_device/MainActivity.java
@@ -0,0 +1,13 @@
+package com.acadgild.s01_a4_exe_real_device;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
